69670b3e6b18533f57851d8a5daa67f8c217c47b
Johannes Stender <johannes.stender@outlook.com>
2019-11-20 12:48:07 +0100

    Added models Added app to project

--- GreenHB/settings.py
+++ GreenHB/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'housingbook.apps.HousingbookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- housingbook/models.py
+++ housingbook/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    society = models.CharField(max_length=200)
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+
+class Apartment(models.Model):
+    code = models.CharField(max_length=200)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+
+class Consumption(models.Model):
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    reading_date = models.DateTimeField('Reading Date')
+    reading_value = models.IntegerField(default=0)
