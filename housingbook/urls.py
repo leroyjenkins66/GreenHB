863019c9578e45c2a5e28dc2e8cd7302e371cd88
Johannes Stender <johannes.stender@outlook.com>
2019-11-20 17:41:04 +0100

    Changed apartment list to my_apartment, which will show apartment detail of the currently signed in user.

--- housingbook/urls.py
+++ housingbook/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='housingbook/index.html'), name='index'),
-    path('apartments/', views.apt_list, name='apt_list'),
+    path('my_apartment/', views.my_apartment, name='my_apartment'),
     path('detail/<int:apartment_id>/', views.detail, name='detail'),
     
     path('privacy/', TemplateView.as_view(template_name='housingbook/privacy.html'), name='privacy'),
--- housingbook/views.py
+++ housingbook/views.py
@@ -4,18 +4,29 @@ from django.contrib.auth.decorators import login_required
 from .models import Apartment, Consumption
 
 @login_required
-def apt_list(request):            
+def my_apartment(request):         
+        #get the apartment just for the current user
         apartments = Apartment.objects.filter( user=request.user )
         
+        if len(apartments) == 0: #use len instead of count to save one database access. count would have generated a SELECT COUNT(*) query, which is extra load on the db.
+            # handle error and ask user to ask operator to link them
+            return render(request, 'housingbook/index.html')
+            
+        #there will only be one apartment.
+        apartment = apartments.first()
+            
+        latest_consumption_list = Consumption.objects.filter( apartment_id=apartment.id ).order_by('-reading_date')#[:5]
+
         context = {
-            'apt_list': apartments,
+            'apartment': apartment,
+            'latest_consumption_list': latest_consumption_list,
         }
         
-        return render(request, 'housingbook/apt_list.html', context)
-   
+        return render(request, 'housingbook/detail.html', context)
+  
+@login_required  
 def detail(request, apartment_id):
     apartment = get_object_or_404(Apartment, pk=apartment_id)
-    latest_consumption_list = Consumption.objects.filter( apartment_id=apartment_id ).order_by('-reading_date')#[:5]
     
     context = {
         'apartment': apartment,
