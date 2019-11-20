ea2d67f3aa964b207a773185be6d1dcc2163c107
Johannes Stender <johannes.stender@outlook.com>
2019-11-20 13:07:21 +0100

    Added string representation of models

--- housingbook/models.py
+++ housingbook/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
 class Owner(models.Model):
-    name = models.CharField(max_length=200)
-    first_name = models.CharField(max_length=200)
-    society = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, blank=True)
+    first_name = models.CharField(max_length=200, blank=True)
+    society = models.CharField(max_length=200, blank=True)
+    
+    def __str__(self):
+        if not self.society:
+            return self.first_name + " " + self.name;
+        else:
+            return self.society;
 
 class Tenant(models.Model):
     name = models.CharField(max_length=200)
     first_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.first_name + " " + self.name;
+
 class Apartment(models.Model):
     code = models.CharField(max_length=200)
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "Apartment " + self.code
 
 class Consumption(models.Model):
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
     reading_date = models.DateTimeField('Reading Date')
     reading_value = models.IntegerField(default=0)
+
