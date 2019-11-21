1ae966cc5d836991d2b45fd5451b375b8e5d2249
Valentin BRETON <valentin.breton@imerir.com>
2019-11-21 11:13:37 +0100

    Changes

--- GreenHB/settings.py
+++ GreenHB/settings.py
@@ -144,5 +144,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sas.greenhb@gmail.com'
-EMAIL_HOST_PASSWORD = '?brbSJqS@,'
+EMAIL_HOST_PASSWORD = get_secret('EMAIL_PASSWORD')
 EMAIL_PORT = 587
