25a42ac01c80fd4f36ea37e79f03c83e1993e60f
Johannes Stender <johannes.stender@outlook.com>
2019-11-21 10:04:53 +0100

    Added basic operator overview

--- housingbook/urls.py
+++ housingbook/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='housingbook/index.html'), name='index'),
+    path('', views.index, name='index'),
     path('my_apartment/', views.my_apartment, name='my_apartment'),
     path('detail/<int:apartment_id>/', views.detail, name='detail'),
+    path('operatoroverview/', views.op_overview, name='op_overview'),
     
     path('privacy/', TemplateView.as_view(template_name='housingbook/privacy.html'), name='privacy'),
 ]
--- housingbook/views.py
+++ housingbook/views.py
@@ -1,36 +1,50 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from .models import Apartment, Consumption
 
 @login_required
-def my_apartment(request):         
-        #get the apartment just for the current user
-        apartments = Apartment.objects.filter( user=request.user )
-        
-        if len(apartments) == 0: #use len instead of count to save one database access. count would have generated a SELECT COUNT(*) query, which is extra load on the db.
-            # handle error and ask user to ask operator to link them
-            return render(request, 'housingbook/index.html')
-            
-        #there will only be one apartment.
-        apartment = apartments.first()
-            
-        latest_consumption_list = Consumption.objects.filter( apartment_id=apartment.id ).order_by('-reading_date')#[:5]
+def index(request):
+    return render(request, 'housingbook/index.html')
 
-        context = {
-            'apartment': apartment,
-            'latest_consumption_list': latest_consumption_list,
-        }
-        
-        return render(request, 'housingbook/detail.html', context)
-  
-@login_required  
-def detail(request, apartment_id):
-    apartment = get_object_or_404(Apartment, pk=apartment_id)
+@login_required
+def my_apartment(request):         
+    #get the apartment just for the current user
+    apartments = Apartment.objects.filter( user=request.user )
     
+    if len(apartments) == 0: #use len instead of count to save one database access. count would have generated a SELECT COUNT(*) query, which is extra load on the db.
+    #TODO: The db might be faster to get the number of rows. Do some profiling
+        # handle error and ask user to ask operator to link them
+        return render(request, 'housingbook/index.html')
+        
+    #there will only be one apartment.
+    apartment = apartments.first()
+        
+    latest_consumption_list = Consumption.objects.filter( apartment_id=apartment.id ).order_by('-reading_date')#[:5]
+
     context = {
         'apartment': apartment,
         'latest_consumption_list': latest_consumption_list,
     }
     
     return render(request, 'housingbook/detail.html', context)
+  
+@staff_member_required  
+def detail(request, apartment_id):
+    apartment = get_object_or_404(Apartment, pk=apartment_id)
+    
+    context = {
+        'apartment': apartment,
+    }
+    
+    return render(request, 'housingbook/detail.html', context)
+    
+@staff_member_required
+def op_overview(request):
+    apartments = Apartment.objects.all()
+    
+    context = {
+        'apartment_list': apartments,
+    }
+    return render(request, 'housingbook/op_overview.html', context)
